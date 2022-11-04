688fa088127226a1a20a6c4cfe6cce814f824fdd
hanbing <742608569@qq.com>
2022-11-04 13:22:09 +0800

    vip 测试

---
+++ Acesys-SpringBoot/src/main/java/com/example/server/controller/vipController.java
@@ -0,0 +1,24 @@
+package com.example.server.controller;
+
+import com.example.server.pojo.RespBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @PROJECT_NAME: Acesys
+ * @DESCRIPTION:
+ * @USER: 韩冰
+ * @DATE: 2022/11/4 0004 13:14
+ */
+@Api(tags = "vipController")
+@RestController
+@RequestMapping("/vip")
+public class vipController {
+
+    @ApiOperation(value = "hello")
+    @GetMapping("hello")
+    public String CanRegistered() {
+        return "hello";
+    }
+}
